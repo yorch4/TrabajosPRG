635317cd5f6df3aba45d462333e5b194204cdcf7
Yorch <Yorch@192.168.1.107>
2018-10-04 17:42:06 +0200

    Bucle3

---
+++ kk/src/Programacion/BucleEjercicio3.java
@@ -0,0 +1,25 @@
+package Programacion;
+
+import javax.swing.JOptionPane;
+
+public class BucleEjercicio3 {
+	
+	public static void main (String [] args) {
+		int numerosNegativos = 0;
+		int numerosPositivos = 0;
+		int i;
+		int a = Integer.parseInt(JOptionPane.showInputDialog("¿Cuantos numeros desea ingresar?"));
+		
+		for (i = 0; i < a; i++) {
+			int num = Integer.parseInt(JOptionPane.showInputDialog("Escribe un numero"));
+			if (num < 0) {
+				numerosNegativos++;
+			}
+			else {
+					numerosPositivos++;
+			}
+		}
+		System.out.println("Numeros positivos: " + numerosPositivos);
+		System.out.println("Numeros negativos: " + numerosNegativos);
+		}
+	}
