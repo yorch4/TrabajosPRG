6fd9d0bc42c018b187fe56dfada25f1b6cfe57d8
diurno <diurno@pc-aula>
2018-10-04 13:42:29 +0200

    laultimita

--- kk/src/Programacion/Bloque3Ejercicio4.java
+++ kk/src/Programacion/Bloque3Ejercicio4.java
@@ -3,9 +3,11 @@ package Programacion;
 import javax.swing.JOptionPane;
 
 public class Bloque3Ejercicio4 {
-	public static void main(String[] args) {		
+	public static void main(String[] args) {	
+		
 	int importe = Integer.parseInt(JOptionPane.showInputDialog("Introduce el importe: "));
 	int cantidadRecibida = Integer.parseInt(JOptionPane.showInputDialog("Introduce la cantidad recibida: "));
+	
 	int devolver = cantidadRecibida - importe;
 	int monedas100 = devolver / 100;
 	int resto100 = devolver % 100;
@@ -16,7 +18,13 @@ public class Bloque3Ejercicio4 {
 	int monedas5 = resto25 / 5;
 	int resto5 = resto25 % 5;
 	int monedas1 = resto5 / 1;
-	System.out.println("La vuelta son:\n " + monedas100 + " monedas de 100\n " + monedas50 + " monedas de 50\n " + monedas25 + " monedas de 25\n " + monedas5 + " monedas de 5\n " + monedas1 + " monedas de 1" );
+	
+	System.out.println("La vuelta son:\n"
+			+ monedas100 + " moneda/s de 100\n"
+					+ monedas50 + " moneda/s de 50\n"
+							+ monedas25 + " moneda/s de 25\n"
+									+ monedas5 + " moneda/s de 5\n"
+											+ monedas1 + " moneda/s de 1" );
 	
 
 }
--- kk/src/Programacion/calculadora.java
+++ kk/src/Programacion/calculadora.java
@@ -4,6 +4,7 @@ import javax.swing.JOptionPane;
 
 public class calculadora {
 	public static void main (String args[]) {
+		
 		String menu = "MENU\n"
 				+ "1.- Hipotenusa de un triangulo\n"
 				+ "2.- Superficie de una circunferencia\n"
@@ -14,36 +15,40 @@ public class calculadora {
 		int num1, num2;
 		
 		int opcion = Integer.parseInt(JOptionPane.showInputDialog(menu));
+
+		String mensaje = "";
 		
 		switch (opcion) {
 		case 1:
 			num1 = Integer.parseInt(JOptionPane.showInputDialog("Introduzca el primer cateto"));
 			num2 = Integer.parseInt(JOptionPane.showInputDialog("Introduzca el segundo cateto"));
-			JOptionPane.showMessageDialog(null, "Resultado: " + (Math.sqrt(Math.pow(num1, 2)+(Math.pow(num2, 2)))));
-			return;
-		
+			mensaje = "Resultado: " + (Math.sqrt(Math.pow(num1, 2)+(Math.pow(num2, 2))));
+			break;
 		case 2:
 			num1 = Integer.parseInt(JOptionPane.showInputDialog("Introduzca el primer numero"));
-			JOptionPane.showMessageDialog(null, "Resultado: " + (Math.PI * Math.pow(num1, 2)));
-			return;
+			mensaje = "Resultado: " + (Math.PI * Math.pow(num1, 2));
+			break;
 			
 		case 3:
 			num1 = Integer.parseInt(JOptionPane.showInputDialog("Introduzca el primer numero"));
-			JOptionPane.showMessageDialog(null, "Resultado: " + (2 * Math.PI * num1));
-			return;
+			mensaje = "Resultado: " + (2 * Math.PI * num1);
+			break;
 			
 		case 4:
 			num1 = Integer.parseInt(JOptionPane.showInputDialog("Introduzca el primer numero"));
 			num2 = Integer.parseInt(JOptionPane.showInputDialog("Introduzca el segundo numero"));
-			JOptionPane.showMessageDialog(null, "Resultado: " + (num1 * num2) );
-			return;
+			mensaje = "Resultado: " + (num1 * num2);
+			break;
 		case 5:
 			num1 = Integer.parseInt(JOptionPane.showInputDialog("Introduzca la base"));
 			num2 = Integer.parseInt(JOptionPane.showInputDialog("Introduzca la altura"));
-			JOptionPane.showMessageDialog(null, "Resultado: " + ((num1 * num2)/2) );
-			return;
+			mensaje = "Resultado: " + ((num1 * num2)/2);
+			break;
 		case 0: System.exit(0);
 		break;
-		}
+		default:
+			mensaje = "Opcion incorrecta, elija bien";
+	}
+		JOptionPane.showInputDialog(null, mensaje);
 }
 }
