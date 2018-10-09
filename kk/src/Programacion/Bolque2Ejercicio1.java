ebfa63fe7e627ff225d6c300b5dd46c2549afc85
diurno <diurno@pc-aula>
2018-10-09 13:06:25 +0200

    cagoento

--- kk/src/Programacion/Bolque2Ejercicio1.java
+++ kk/src/Programacion/Bolque2Ejercicio1.java
@@ -5,16 +5,18 @@ import javax.swing.JOptionPane;
 public class Bolque2Ejercicio1 {
 	
 	public static void main (String [] args) {
+		int suma = 0;
 		int media = 0;
 		int i;
 		int a = Integer.parseInt(JOptionPane.showInputDialog("¿Cuantos numeros desea ingresar?"));
 		
 		for (i = 0; i < a; i++) {
 			int num = Integer.parseInt(JOptionPane.showInputDialog("Escribe un numero"));
+			suma = suma + num;
 			
-			media = (num + num)/a;
 		
 	}
+		media = suma/a;
 		System.out.println("La media es: " + media);
 	}
 }
