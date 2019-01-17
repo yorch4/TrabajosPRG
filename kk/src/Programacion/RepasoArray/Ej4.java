dfeccd9a46082f03ba4e5cc0734ebb0fc8dcdb9e
Yorch <Yorch@192.168.1.110>
2019-01-17 19:34:48 +0100

    ejArray

--- kk/src/Programacion/RepasoArray/Ej4.java
+++ kk/src/Programacion/RepasoArray/Ej4.java
@@ -1,5 +1,7 @@
 package Programacion.RepasoArray;
 
+import javax.swing.JOptionPane;
+
 public class Ej4 {
 	public static int FILAS = 5;
 	public static int COLUMNAS = 5;
@@ -9,25 +11,26 @@ public class Ej4 {
 	public static void main(String[] args) {
 		int matriz[][] = creaMatrizAzar();
 		print(matriz);
-		matrizDispersa(matriz);
+		eliminarFila(matriz);
 	}
 	
 	public static int[][] creaMatrizAzar() {
 		
-	//	int  matriz[][] = new int[FILAS][COLUMNAS];
+		int  matriz[][] = new int[FILAS][COLUMNAS];
 		
-	//	for (int i = 0; i < FILAS; i++) {
-	//		for (int j = 0; j < COLUMNAS; j++) {
-	//			matriz[i][j] = (int) (Math.round(Math.random() * (100 - (-100))+(-100)));
-	//		}
-	//	}
-		int  matriz[][] = new int[][]{
+		for (int i = 0; i < FILAS; i++) {
+			for (int j = 0; j < COLUMNAS; j++) {
+				matriz[i][j] = (int) (Math.round(Math.random() * (100 - (-100))+(-100)));
+			}
+		}
+		//Matriz modificada
+	/**	int  matriz[][] = new int[][]{
 			{0,		8, 		2, 		5, 		7},
-			{1, 	0, 		3,		4,		8},
-			{0, 	8, 		0, 		8, 		0},
-			{8, 	5, 		8	, 	0, 		8},
-			{8, 	8, 		8, 		8, 		0}
-		};
+			{8, 	0, 		3,		8,		8},
+			{2, 	3, 		0, 		8, 		0},
+			{5, 	4, 		8	, 	0, 		8},
+			{7, 	8, 		0, 		8, 		0}
+	**/
 		return matriz;
 	}
 	
@@ -38,6 +41,7 @@ public class Ej4 {
 			}
 			System.out.println();
 		}
+		System.out.println();
 	}
 	
 	public static void matrizPositiva(int matriz[][]) {
@@ -122,6 +126,89 @@ public class Ej4 {
 			System.out.println("La matriz ES dispersa");
 		} else {
 			System.out.println("La matriz NO es dispersa");
+		}
+	}
+	
+	public static void arrayUnidimensional(int matriz[][]) {
+		int indiceArray = FILAS * COLUMNAS;
+		int variableArray = 0;
+		int array[] = new int [indiceArray];
+		
+		for (int i = 0; i < FILAS; i++) {
+			for (int j = 0; j < COLUMNAS; j++) {
+				array[variableArray] = matriz[i][j];
+				variableArray++;
+			}
+		}
+		for (int i = 0; i < indiceArray; i++) {
+			System.out.print(array[i] + " ");
+		}
+	}
+	
+	public static void matrizSimetrica(int matriz[][]) {
+		boolean matrizSimetrica = true;
+		
+		for (int i = 0; i < FILAS; i++) {
+			for (int j = 0; j < COLUMNAS; j++) {
+				if (matriz[i][j] != matriz[j][i]) {
+					matrizSimetrica = false;
+				}
+			}
+		}
+		if (matrizSimetrica == true) {
+			System.out.println("La matriz ES simetrica");
+		} else {
+			System.out.println("La matriz NO es simetrica");
+		}
+	}
+	
+	public static void matrizTraspuesta(int matriz[][]) {
+		int matrizTraspuesta[][] = creaMatrizAzar();
+		for (int i = 0; i < FILAS; i++) {
+			for (int j = 0; j < COLUMNAS; j++) {
+				matrizTraspuesta[i][j] = matriz[j][i];
+			}
+		}
+		
+		for (int i = 0; i < FILAS; i++) {
+			for (int j = 0; j < COLUMNAS; j++) {
+				System.out.print(matrizTraspuesta[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void matrizOpuesta(int matriz[][]) {
+		for (int i = 0; i < FILAS; i++) {
+			for (int j = 0; j < COLUMNAS; j++) {
+				matriz[i][j] = matriz[i][j] * (-1);
+			}
+		}
+		print(matriz);
+	}
+	
+	public static void eliminarFila(int matriz[][]) {
+		int  matrizSinFila[][] = new int[FILAS - 1][COLUMNAS];
+		int filaEliminar =Integer.parseInt(JOptionPane.showInputDialog("Pon la fila que quieres eliminar"));
+		int indicePrueba = 0;
+		
+		for (int i = 0; i < FILAS; i++, indicePrueba++) {
+			for (int j = 0; j < COLUMNAS; j++) {
+				if (filaEliminar == i) {
+					i++;
+				}
+				if (i > 4) {
+					break;
+				}
+					matrizSinFila[indicePrueba][j] = matriz[i][j];
+			}	
+		}
+		
+		for (int i = 0; i < FILAS - 1; i++) {
+			for (int j = 0; j < COLUMNAS; j++) {
+				System.out.print(matrizSinFila[i][j] + "\t");
+			}
+			System.out.println();
 		}
 	}
 
