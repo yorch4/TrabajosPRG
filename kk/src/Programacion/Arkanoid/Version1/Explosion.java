c31ad2c15d97f4d3a7990271313893dc11388fe4
diurno <diurno@pc-aula>
2019-01-28 14:56:24 +0100

    ark vr2

---
+++ kk/src/Programacion/Arkanoid/Version1/Explosion.java
@@ -0,0 +1,13 @@
+package Programacion.Arkanoid.Version1;
+
+public class Explosion extends Objetos {
+
+	public Explosion(Stage stage) {
+		super(stage);
+		setSpriteName( new String[] {"explosion1.png","explosion2.png","explosion3.png","explosion1.png","explosion2.png","explosion3.png"});
+		setFrameSpeed(35);
+	}
+
+
+
+}
